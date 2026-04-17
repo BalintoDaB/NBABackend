51f03186777e0f947e519ccadecc828cd2bdd322
Balinto <balintm2006@gmail.com>
2026-04-17 08:38:21 +0000

    Update cors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const routes = require('./routes');
 
+app.use(cors({
+    origin: 'https://nbastars.jcloud.jedlik.cloud',
+}));
 app.use(express.json());
 
 app.use('/api', routes)
